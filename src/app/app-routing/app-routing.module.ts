ebf29587f3d6431dce688351d8b4a36b16f068b3
Luke Stevens <luke.stevens@deltadna.com>
2019-01-21 15:10:38 +0000

    Added routing for news component

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from '../components/landing-page/landing-page.component';
+import { NewsComponent } from '../components/news/news.component';
 
 const routes: Routes = [
-  { path: '', component: LandingPageComponent}
+  { path: '', component: LandingPageComponent},
+  { path: 'news', component: NewsComponent}
 ];
 
 @NgModule({
