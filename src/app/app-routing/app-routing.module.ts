28fb16fdb4bcd64b2ff2caf567fc978522d98072
lukecmstevens <lcm.stevens96@gmail.com>
2019-01-21 21:58:04 +0000

    Add placeholder about component

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from '../components/landing-page/landing-page.component';
 import { NewsComponent } from '../components/news/news.component';
 import { ArticleComponent } from '../components/article/article.component';
+import { AboutComponent } from '../components/about/about.component';
+import { FixturesComponent } from '../components/fixtures/fixtures.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent},
+  { path: 'about', component: AboutComponent},
   { path: 'news', component: NewsComponent},
-  { path: 'article/:id', component: ArticleComponent}
+  { path: 'article/:id', component: ArticleComponent},
+  { path: 'fixtures', component: FixturesComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { NewsComponent } from './components/news/news.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { ArticleComponent } from './components/article/article.component';
+import { AboutComponent } from './components/about/about.component';
+import { FixturesComponent } from './components/fixtures/fixtures.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewsComponent,
     LandingPageComponent,
-    ArticleComponent
+    ArticleComponent,
+    AboutComponent,
+    FixturesComponent
   ],
   imports: [
     BrowserModule,
