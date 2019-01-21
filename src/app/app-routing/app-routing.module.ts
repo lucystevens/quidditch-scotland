1547b6b22fe633cdb92b7139e7b4c8d416484c3a
Luke Stevens <luke.stevens@deltadna.com>
2019-01-21 13:52:23 +0000

    Add routing to landing page component

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from '../components/landing-page/landing-page.component';
+
+const routes: Routes = [
+  { path: '', component: LandingPageComponent}
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
