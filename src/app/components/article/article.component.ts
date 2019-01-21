061f2ff208f47123d26de11260676a8a29aad1d1
Luke Stevens <luke.stevens@deltadna.com>
2019-01-21 15:46:55 +0000

    Add component for viewing individual articles

---
+++ src/app/components/article/article.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/domain/data-definitions';
+import { ArticleService } from 'src/app/services/article.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  article: Article;
+
+  constructor(
+    private route: ActivatedRoute,
+    private articleService: ArticleService) { }
+
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.article = this.articleService.getArticle(id);
+  }
+
+}
