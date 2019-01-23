315ce01d3134b548fd2b18ea6ef034a6dadf314d
Luke Stevens <luke.stevens@deltadna.com>
2019-01-23 15:27:06 +0000

    Add sample player

--- src/app/domain/players.ts
+++ src/app/domain/players.ts
@@ -1,3 +1,12 @@
 import { Player } from "./data-definitions";
 
-export const PLAYERS : Player[] = [];
+export const PLAYERS : Player[] = [
+    {
+        name : "Luke Stevens",
+        positions : ["Beater"],
+        startedPlaying : new Date(2014, 8),
+        number : 98,
+        photo : "https://ca.slack-edge.com/T03NSHATC-UBHF98WMT-c93f5dd90488-512",
+        info : "I made this website"
+    }
+];
