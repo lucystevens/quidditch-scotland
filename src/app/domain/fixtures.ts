9d94353cea13e0508116b5c0ae5d87f9591939e6
Luke Stevens <lcm.stevens96@gmail.com>
2019-03-19 12:48:56 +0000

    Misc fixes

--- src/app/domain/fixtures.ts
+++ src/app/domain/fixtures.ts
@@ -4,13 +4,13 @@ export const FIXTURES : Fixture[] = [
     {
         opposition: "Team Catalonia",
         score: "70* - 50",
-        date: new Date(2018, 11, 18),
+        date: new Date(2018, 10, 18),
         location: "Barcelona, Spain"
     },
     {
         opposition: "Team Catalonia",
         score: "150 - 50*",
-        date: new Date(2018, 11, 18),
+        date: new Date(2018, 10, 18),
         location: "Barcelona, Spain"
     }
 ];
